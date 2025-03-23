371d2fc1e0262f0ce184f0ffdea3602fbc562f07
Sinchu-XD <abhiyanshicreation@gmail.com>
2025-03-23 23:19:16 +0530

    Update Abhi.py

--- Abhi.py
+++ Abhi.py
@@ -10,10 +10,10 @@ from pyrogram import Client, filters
 logging.basicConfig(level=logging.INFO)
 
 # Your API credentials
-API_ID = "YOUR_API_ID"
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
+API_ID = 25024171  # Get from my.telegram.org
+ API_HASH = "7e709c0f5a2b8ed7d5f90a48219cffd3"
+ BOT_TOKEN = "7726535663:AAFVBNgn5z-gUK7Tr7XoKTS3bopW3OLBSPM"
+ CHAT_ID = -4752836661
 
 # Initialize Pyrogram Client
 app = Client("cricket_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
